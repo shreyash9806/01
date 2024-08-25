7f9bd4f29a68ccd4eb1cdfb118bdb88131048d95
shreyash9806 <skumar60_be18@thapar.edu>
2024-08-26 04:09:20 +0530

    commit

---
+++ app/login/page.js
@@ -0,0 +1,9 @@
+
+function page() {
+  return (
+    <div className="text-center border-2 border-white
+    h-[100vh] flex items-center justify-center">Login page</div>
+  )
+}
+
+export default page
